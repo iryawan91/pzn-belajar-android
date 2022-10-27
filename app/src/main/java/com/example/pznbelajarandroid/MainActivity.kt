1d3877c2874c6d1e69020171e3b43a55fdbaf3fc
iryawan91 <iryawan1991@gmail.com>
2022-10-27 19:27:56 +0700

    add id View

--- app/src/main/java/com/example/pznbelajarandroid/MainActivity.kt
+++ app/src/main/java/com/example/pznbelajarandroid/MainActivity.kt
@@ -1,11 +1,20 @@
-package com.example.pznbelajarandroid
+package com.example.belajarandroiddasar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.hello_world)
+
+        val nameEditText: EditText=findViewById(R.id.nameEditText)
+        val sayButton: Button=findViewById(R.id.sayButton)
+        val sayTextView: TextView=findViewById(R.id.sayTextView)
+
+        sayTextView.text="Hallo!"
     }
 }
